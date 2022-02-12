a9a90411e8c487ad96776b166379f86d5b373eb4
Rahul Kumar <rahulrai2822march@gmail.com>
2022-02-12 16:27:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const port = 80;
+const app = express();
+//Express stuffs
+app.use('/static',express.static('static'));
+app.use(express.urlencoded())
+//PUG stuffs
+app.set('view engine','pug');//set the template engine as pug
+app.set('views',path.join(__dirname,'views'));//set the views directory
+
+app.get('/',(req,res) => {
+    const params = {};
+    res.status(200).render('index.pug',params)
+;
+});
+
+app.listen(port , ()=>{console.log(`Application running on port ${port}`)});
