43e137e27daa43566caa6faf2008b045556ae97a
Rahul Kumar <rahulrai2822march@gmail.com>
2025-05-04 14:02:36 +0530

    comments added

--- build/app.js
+++ build/app.js
@@ -12,7 +12,7 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/test');
 }
-console.log("TEST PR")
+
 // Define mongoose schema
 var contactSchema = new mongoose.Schema({
     name : String,
